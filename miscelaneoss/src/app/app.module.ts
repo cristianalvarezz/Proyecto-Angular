e4d469d45a4c711b12483209245e548687a3fe63
cristianalvarezz <alvarescristian46@gmail.com>
2021-06-02 16:47:32 -0500

    cambio de estilos y trabajos con los ng

--- miscelaneoss/src/app/app.module.ts
+++ miscelaneoss/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
