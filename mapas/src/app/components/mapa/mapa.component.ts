2f6a22100b74446fd79a4e3a5fcd27ff23854b98
cristianalvarezz <alvarescristian46@gmail.com>
2021-06-15 14:01:53 -0500

    Agregar y eliminar marcador

--- mapas/src/app/components/mapa/mapa.component.ts
+++ mapas/src/app/components/mapa/mapa.component.ts
@@ -1,27 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Marcador } from 'src/app/classes/marcador.class';
-
-
-
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.css']
+  styleUrls: ['./mapa.component.css'],
 })
 export class MapaComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
-  marcadores:Marcador[]=[];
-  constructor() { 
+  marcadores: Marcador[] = [];
+  constructor(private snackBar: MatSnackBar) {
+    // const nuevoMarcado = new Marcador(51.678418, 7.809007);
+    // this.marcadores.push(nuevoMarcado);
 
-    const nuevoMarcado =new Marcador(51.678418,7.809007);
+    //si existen marcadores en el storage
+    if(localStorage.getItem('marcadores')){
+     // hacemos la inversa 
+      this.marcadores = JSON.parse(localStorage.getItem('marcadores')||'{}');
+    }
+  }
+  ngOnInit(): void {}
+
+  agregarMarcador(evento: any) {
+    //aggaro las cordenadas enviadas al hacer click en cualquier parte del mapa 
+    const coords: { lat: number; lng: number } = evento.coords;
+    const nuevoMarcado = new Marcador(coords.lat, coords.lng);
     this.marcadores.push(nuevoMarcado);
+    this.guardarStorage();
+    this.snackBar.open('Marcador agregado', 'Cerrar',{duration:3000});
   }
-  ngOnInit(): void {
- 
-  }
- 
-  
 
+  guardarStorage(){
+    //transformo el arreglo de marcadores en un json 
+    localStorage.setItem('marcadores', JSON.stringify(this.marcadores));
+    
+  }
+  borrarMarcador(i:number){
+    // console.log(i);
+    this.marcadores.splice(i,1);
+    this.guardarStorage();
+    this.snackBar.open('Marcador Borrado', 'Cerrar',{duration:3000});
+    
+  }
 }
--- mapas/src/shared/material.module.ts
+++ mapas/src/shared/material.module.ts
@@ -1,28 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatSelectModule} from '@angular/material/select';
+
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
     MatCardModule,
     MatToolbarModule,
-    MatProgressBarModule,MatButtonModule
+    MatProgressBarModule,MatButtonModule,MatSnackBarModule
 
   ],
   exports: [
     MatCardModule,
-    MatToolbarModule,MatProgressBarModule,MatButtonModule
+    MatToolbarModule,MatProgressBarModule,MatButtonModule,MatSnackBarModule
   ]
 })
 
