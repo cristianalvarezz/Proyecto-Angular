a06e06ec8b23851021fe9cb5c8118e4596bd3e5a
cristianalvarezz <alvarescristian46@gmail.com>
2021-06-22 17:30:02 -0500

    Autentificacion JWT y creacion de token

--- crudBACK/src/main/java/com/tutorial/crud/security/jwt/JwtTokenFilter.java
+++ crudBACK/src/main/java/com/tutorial/crud/security/jwt/JwtTokenFilter.java
@@ -1,4 +1,58 @@
 package com.tutorial.crud.security.jwt;
 
-public class JwtTokenFilter {
+import com.tutorial.crud.security.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//esta ckase comprobara si hay un token valido
+//si no devolvera una respuesta diciendo que no esta autorizado
+//se va a ejecutar una vez por cada peticion
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    //comprueba que el token sea autorizado y si sirva
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(req);
+            if(token != null && jwtProvider.validateToken(token)){
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(nombreUsuario);
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e){
+            logger.error("fail en el método doFilter " + e.getMessage());
+        }
+        filterChain.doFilter(req, res);
+    }
+
+    //extrae el token y elimina partes de este  
+    private String getToken(HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer"))
+            return header.replace("Bearer ", "");
+        return null;
+    }
 }
--- crudBACK/src/main/java/com/tutorial/crud/security/service/UserDetailsServiceImpl.java
+++ crudBACK/src/main/java/com/tutorial/crud/security/service/UserDetailsServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 
 public class UserDetailsServiceImpl implements UserDetailsService {
- 
+
     @Autowired
     UsuarioService usuarioService;
 
