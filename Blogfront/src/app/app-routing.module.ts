cbb4f6fbdff906892f831c28d0c0744bb3eddea3
cristianalvarezz <alvarescristian46@gmail.com>
2021-06-23 16:50:17 -0500

    Registro de usuario

--- Blogfront/src/app/app-routing.module.ts
+++ Blogfront/src/app/app-routing.module.ts
@@ -7,17 +7,17 @@ import { EditarProductoComponent } from './producto/editar-producto.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistroComponent } from './auth/registro/registro.component';
-
+import { ProdGuardService as guard } from './guard/prod-guard.service';
 
 const routes: Routes = [
-  {path:'',component:IndexComponent},
-  {path:'login',component:LoginComponent},
+  { path: '', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
-  {path: 'lista', component: ListaProductoComponent},
-  {path: 'detalle/:id', component: DetalleProductoComponent},
-  {path: 'nuevo', component: NuevoProductoComponent},
-  {path: 'editar/:id', component: EditarProductoComponent},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: 'lista', component: ListaProductoComponent, canActivate: [guard], data: { expectedRol: ['admin', 'user'] } },
+  { path: 'detalle/:id', component: DetalleProductoComponent, canActivate: [guard], data: { expectedRol: ['admin', 'user'] } },
+  { path: 'nuevo', component: NuevoProductoComponent, canActivate: [guard], data: { expectedRol: ['admin'] } },
+  { path: 'editar/:id', component: EditarProductoComponent, canActivate: [guard], data: { expectedRol: ['admin'] } },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
--- Blogfront/src/app/guard/prod-guard.service.ts
+++ Blogfront/src/app/guard/prod-guard.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { TokenService } from '../service/token.service';
+
+//proteje las url para que cualquiera no pueda ingresar
 @Injectable({
   providedIn: 'root'
 })
-export class ProdGuardService {
+export class ProdGuardService implements CanActivate {
 
-  constructor() { }
+  realRol!: string;
+
+  constructor(
+    private tokenService: TokenService,
+    private router: Router
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const expectedRol = route.data.expectedRol;
+    const roles = this.tokenService.getAuthorities();
+    this.realRol = 'user';
+    roles.forEach(rol => {
+      if (rol === 'ROLE_ADMIN') {
+        this.realRol = 'admin';
+      }
+    });
+    //a la hora de ingresar si soy usuario no me dejara entrar he ingresare al principal
+    //si estamos sin role o tenemos un rol no esperado notenemos acceso a las cosas de administrador
+    if (!this.tokenService.getToken() || expectedRol.indexOf(this.realRol) === -1) {
+      this.router.navigate(['/']);
+      return false;
+    }
+    return true;
+  }
 }
