dc1089a1b3eca7beb69c3f792b35cda5c49bf61a
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-01 14:20:46 -0500

    rutas hijas, pagina 404,login,pagina de registro

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -6,14 +6,21 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'grafica1', component: Grafica1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
 
-    
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 
--- adminpro/src/app/pages/nopagefound/nopagefound.component.ts
+++ adminpro/src/app/pages/nopagefound/nopagefound.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: [
-  ]
+  styleUrls: [ './nopagefound.component.css' ]
 })
 export class NopagefoundComponent implements OnInit {
 
@@ -12,5 +11,7 @@ export class NopagefoundComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  year = new Date().getFullYear();
+
 
 }
