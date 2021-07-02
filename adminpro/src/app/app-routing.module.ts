aa0ff609910ea70f7330e90ac036354fe5223145
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-02 12:07:17 -0500

    dashboar por encima de todo

--- adminpro/src/app/app-routing.module.ts
+++ adminpro/src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authRoutingModule } from './auth/auth.routing';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-
 import { PagesRoutingModule } from './pages/pages.routing';
 
 
 const routes: Routes = [
 
-  //path: '/dashboard' PagesRouting
-  //path : '/auth' AuthRouting 
+  //path:'/dashboard' PagesRouting
+  //path :'/auth' AuthRouting 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
