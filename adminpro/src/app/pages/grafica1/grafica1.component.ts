a613d3f62667522ae37b08eb5b80c4de5156483d
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-04 16:00:45 -0500

    graficas

--- adminpro/src/app/pages/grafica1/grafica1.component.ts
+++ adminpro/src/app/pages/grafica1/grafica1.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class Grafica1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Grafica1Component {
+  public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public labels2: string[] = ['Tampico', 'Bebida caliente', 'Tamal'];
+  public data1 = [[10, 15, 40]];
+  public data2 = [[10, 30, 50]];
 }
