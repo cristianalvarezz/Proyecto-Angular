7b06f2a7c04dfd83fccf141f15a5737e8d4ef8fb
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-14 18:12:19 -0500

    busqueda por tabla

--- backend-server/controllers/busquedas.js
+++ backend-server/controllers/busquedas.js
@@ -22,7 +22,42 @@ const getTodo = async (req, res = response) => {
     hospitales,
   });
 };
+const getDocumentosColeccion = async (req, res = response) => {
+  const busqueda = req.params.busqueda;
+  const tabla = req.params.tabla;
+  console.log(busqueda);
+  console.log(tabla);
+  //uso la exprecion regular para que me traiga los resultados aproximados
+  const regex = new RegExp(busqueda, "i");
+  let data = [];
+
+  switch (tabla) {
+    case "medicos":
+      data = await Medico.find({ nombre: regex })
+        .populate("usuario", "nombre img")
+        .populate("hospital", "nombre img");
+      break;
+    case "hospitales":
+      data = await Hospital.find({ nombre: regex }).populate(
+        "usuario",
+        "nombre img"
+      );
+      break;
+    case "usuarios":
+      data = await Usuario.find({ nombre: regex });
+      break;
+    default:
+      return res.status(400).json({
+        msg: "La tabla tiene que ser usuarios/medicos/hospitales",
+      });
+  }
+  res.json({
+    ok: true,
+    resultados: data,
+  });
+};
 
 module.exports = {
   getTodo,
+  getDocumentosColeccion,
 };
--- backend-server/routes/busquedas.js
+++ backend-server/routes/busquedas.js
@@ -3,8 +3,9 @@
 const { Router } = require("express");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const router = Router();
-const { getTodo } = require("../controllers/busquedas");
+const { getTodo, getDocumentosColeccion } = require("../controllers/busquedas");
 
 router.get("/:busqueda", validarJWT, getTodo);
+router.get("/coleccion/:tabla/:busqueda", validarJWT, getDocumentosColeccion);
 
 module.exports = router;
