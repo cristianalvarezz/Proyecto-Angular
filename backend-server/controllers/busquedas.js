16c3722e10ca26b4fe5d5a9822548564d8d104ca
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-14 17:42:42 -0500

    busqueda de todos

--- backend-server/controllers/busquedas.js
+++ backend-server/controllers/busquedas.js
@@ -1,13 +1,25 @@
 const { response } = require("express");
-
+const Usuario = require("../models/usuario");
+const Medico = require("../models/medico");
+const Hospital = require("../models/hospital");
 // getTodo
 
-const getTodo = (req, res=response) => {
-const busqueda =req.params.busqueda;
+const getTodo = async (req, res = response) => {
+  const busqueda = req.params.busqueda;
+  //uso la exprecion regular para que me traiga los resultados aproximados
+  const regex = new RegExp(busqueda, "i");
+
+  const [usuarios, medicos, hospitales] = Promise.all([
+    Usuario.find({ nombre: regex }),
+    Medico.find({ nombre: regex }),
+    Hospital.find({ nombre: regex }),
+  ]);
 
   res.json({
     ok: true,
-    busqueda: busqueda,
+    usuarios,
+    medicos,
+    hospitales,
   });
 };
 
