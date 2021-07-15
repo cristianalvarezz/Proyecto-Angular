338d6c1f9263310cb95e79c4b6f25622c05c3d61
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-15 12:46:43 -0500

    verificacion de token dado por google sign in

--- backend-server/controllers/auth.js
+++ backend-server/controllers/auth.js
@@ -1,57 +1,73 @@
-const { response } = require('express');
-const bcrypt = require('bcryptjs');
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
 
-const Usuario = require('../models/usuario');
-const { generarJWT } = require('../helpers/jwt');
+const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
+const login = async (req, res = response) => {
+  //atrapo el email y password
+  const { email, password } = req.body;
 
-const login = async( req, res = response ) => {
+  try {
+    // Verificar email
+    const usuarioDB = await Usuario.findOne({ email });
 
-    //atrapo el email y password
-    const { email, password } = req.body;
-
-    try {
-        
-        // Verificar email
-        const usuarioDB = await Usuario.findOne({ email });
-
-        if ( !usuarioDB ) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'Email no encontrado'
-            });
-        }
-
-        // Verificar contraseña
-        const validPassword = bcrypt.compareSync( password, usuarioDB.password );
-        if ( !validPassword ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'Contraseña no válida'
-            });
-        }
-
-        // Generar el TOKEN - JWT
-        const token = await generarJWT( usuarioDB.id );
-
-
-        res.json({
-            ok: true,
-            token
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Hable con el administrador'
-        })
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Email no encontrado",
+      });
     }
 
+    // Verificar contraseña
+    const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Contraseña no válida",
+      });
+    }
 
-}
+    // Generar el TOKEN - JWT
+    const token = await generarJWT(usuarioDB.id);
 
+    res.json({
+      ok: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
+
+const googleSignIn = async (req, res = response) => {
+  //recibo el token donde obtendre un email, un nombre y una imagen 
+    const googleToken = req.body.token;
+    //el await es para que se espere antes de hacer cualquier cosa 
+    
+    try {
+    //con esto podre mandar el token verificarlo y recuperar email,nombre y imagen 
+   const{name,email,picture} =  await googleVerify(googleToken);
+    res.json({
+      ok: true,
+      msg: "Google Signin",
+      name,email,picture
+    });
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      msg: "token no es correcto  ",
+   
+    });
+  }
+};
 
 module.exports = {
-    login
-}
+  login,
+  googleSignIn,
+};
--- backend-server/routes/auth.js
+++ backend-server/routes/auth.js
@@ -2,7 +2,7 @@
     Path: '/api/login'
 */
 const { Router } = require("express");
-const { login} =require("../controllers/auth");
+const { login,googleSignIn} =require("../controllers/auth");
 const { check }=require('express-validator');
 const { validarCampos } = require("../middlewares/validar-campos");
 
@@ -13,6 +13,13 @@ router.post('/',[
     check('email','El email es obligatorio ').isEmail(),
     check('password','El password es obligatorio ').not().isEmpty(),
     validarCampos
-],login)
+],login);
+
+
+router.post('/google',[
+    //el token sera obligatorio
+    check('token','El token de Google es obligatorio ').not().isEmpty(),
+    validarCampos
+],googleSignIn)
 
 module.exports =router;
