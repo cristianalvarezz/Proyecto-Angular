3ff887d67eb58eff978256d48e0b7d6dc1bd297c
cristianalvarezz <alvarescristian46@gmail.com>
2021-07-30 11:43:48 -0500

    conexion a sockets

--- server/classes/server.ts
+++ server/classes/server.ts
@@ -1,19 +1,44 @@
 //npm install @types/express
 import express from "express";
 import { SERVER_PORT } from "../global/environment";
+import socketIO from 'socket.io';
+import http from 'http'
 
 //esto por que va a ser lo unico que se exportara de este archivo
 export default class Server {
   public app: express.Application;
   public port: number;
 
+
+  //socket io
+  
+  public io:socketIO.Server;
+  private httpServer:http.Server;
+
   constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+
+    this.httpServer=new http.Server(this.app);
+
+    //a qui recibe el http server
+    this.io=socketIO(this.httpServer);
   }
 
+
+  private escucharSokets(){
+    console.log("EScuchando conexiones -sockets");
+
+    //para saber cuando una persona se conecta mediante sockets
+  //io es nuestro servidor de sockets 
+  //on es para escuchar algun evento
+  //el cliente es el nuevo dispositivo que se conecta a nuestra conexion con sockets
+    this.io.on('connection',cliente=>{
+      console.log('Cliente conectado')
+    })
+  }
   //metodo para levantar servidor
   start(callback:any) {
-    this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }
--- server/dist/classes/server.js
+++ server/dist/classes/server.js
@@ -6,15 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //npm install @types/express
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../global/environment");
+const socket_io_1 = __importDefault(require("socket.io"));
+const http_1 = __importDefault(require("http"));
 //esto por que va a ser lo unico que se exportara de este archivo
 class Server {
     constructor() {
         this.app = express_1.default();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        //a qui recibe el http server
+        this.io = socket_io_1.default(this.httpServer);
     }
     //metodo para levantar servidor
     start(callback) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
 exports.default = Server;
