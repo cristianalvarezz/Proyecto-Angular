42ac4395d2c467f590483096e2133818b5030bb9
cristianalvarezz <alvarescristian46@gmail.com>
2021-11-15 13:10:31 -0500

    primer commit

---
+++ store/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    SharedModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ store/src/app/product/components/product-detail/product-detail.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ProductsService } from './../../../core/services/products/products.service';
+import { Product } from './../../../core/models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+
+  product: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+     // this.product = this.productsService.getProduct(id);
+     this.fetchProduct(id);
+    });
+  }
+
+  fetchProduct(id:any){
+    this.productsService.getProduct(id)
+        .subscribe(product=>{
+          this.product=product
+          console.log(product)
+        })
+  }
+  createProduct(){
+    const newProduct:Product={
+      id: '222',
+      title: 'Nuevo desde angular',
+      price: 2134,
+      description: 'nuevo producto',
+      image: 'assets/images/banner-1.jpg'
+    }
+    this.productsService.createProduct(newProduct)
+        .subscribe(product=>{
+          this.product=product
+          console.log(product)
+        })
+  }
+
+ updateProduct() {
+    const updateProduct: Partial<Product> = {
+      price: 555555,
+      description: 'edicion titulo'
+    };
+    this.productsService.updateProduct('2', updateProduct)
+    .subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  deleteProduct() {
+    this.productsService.deleteProduct('222')
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
+}
